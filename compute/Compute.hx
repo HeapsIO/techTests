c4991e196c3b44f28ec34cc0cf83654d470c8192
Nicolas Cannasse <ncannasse@gmail.com>
2024-01-08 01:52:24 +0100

    compute texture sample

--- compute/Compute.hx
+++ compute/Compute.hx
@@ -10,21 +10,32 @@ class BufferView extends hxsl.Shader {
 
 }
 
-class ComputeShader extends hxsl.Shader {
+class ComputeBufShader extends hxsl.Shader {
 	static var SRC = {
 		@:import h3d.shader.BaseMesh;
 		@param var buffer : RWBuffer<Vec4,1>;
 		@param var color : Vec4;
 		function main() {
-			setLayout(8,1,1);
 			buffer[0] = color + vec4(sin(global.time), 0, 0, 0);
+		}
+	}
+}
+
+class ComputeTexShader extends hxsl.Shader {
+	static var SRC = {
+		@:import h3d.shader.BaseMesh;
+		@param var tex : RWTexture2D<Vec4>;
+		@param var color : Vec4;
+		function main() {
+			tex[computeVar(GlobalInvocation).xy] = vec4(vec2(computeVar(GlobalInvocation).xy) / tex.size(), 0, 1);
 		}
 	}
 }
 
 class Compute extends hxd.App {
 
-	var shader : ComputeShader;
+	var bufShader : ComputeBufShader;
+	var texShader : ComputeTexShader;
 
 	override function init() {
 		engine.backgroundColor = 0xFF200020;
@@ -48,13 +59,26 @@ class Compute extends hxd.App {
 		bmp.addShader(sh);
 		bmp.blendMode = None;
 
-		shader = new ComputeShader();
-		shader.buffer = buffer;
+		bufShader = new ComputeBufShader();
+		bufShader.buffer = buffer;
+
+		var wtex = new h3d.mat.Texture(256,256,[Writable]);
+		texShader = new ComputeTexShader();
+		texShader.tex = wtex;
+
+		var bmp = new h2d.Bitmap(h2d.Tile.fromTexture(wtex), s2d);
+		bmp.x = 400;
+		bmp.y = 100;
+		bmp.blendMode = None;
 	}
 
 	override function render(e:h3d.Engine) {
-		shader.color.set(0,1,0.5*Math.random(),1);
-		s3d.renderer.computeDispatch(shader);
+		bufShader.color.set(0,1,0.5*Math.random(),1);
+		s3d.renderer.computeDispatch(bufShader);
+
+		texShader.color.set(0,1,0.5*Math.random(),1);
+		s3d.renderer.computeDispatch(texShader,Std.int(texShader.tex.width * 0.75), Std.int(texShader.tex.height*0.75));
+
 		super.render(e);
 	}
 
