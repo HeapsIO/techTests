37f5614fdb686bed7c6e88ba1bd748b972a14689
Nicolas Cannasse <ncannasse@gmail.com>
2025-01-26 11:53:56 +0100

    working multi/sub components

--- domkitEditor/DomkitEditor.hx
+++ domkitEditor/DomkitEditor.hx
@@ -1,15 +1,61 @@
+import hrt.impl.DomkitViewer;
+
+class Base extends h2d.Flow implements h2d.domkit.Object {
+}
+
+/*
+@:source("comp/button")
+class Button extends Base {
+	public function new(text:String,?parent) {
+		super(parent);
+		initComponent();
+	}
+}
+
+@:source("comp/window")
+class Window extends Base {
+
+	public var show = true;
+
+}
+
+@:source("comp/window2.special") class Special extends Base {
+
+	public function new(title:String,?parent) {
+		super(parent);
+		initComponent();
+	}
+
+}
+
+@:source("comp/window2")
+class Window2 extends Window {
+
+	public var text = "NewWindow2";
+}
+*/
 
 class DomkitEditor extends hxd.App {
 
-	var style : h2d.domkit.Style;
+	var style : DomkitStyle;
+	var root : h2d.Flow;
 
 	override function init() {
-		style = new h2d.domkit.Style();
+		style = new DomkitStyle();
+		style.loadDefaults([hxd.Res.vars]);
 		style.load(hxd.Res.style);
 		style.allowInspect = true;
-		var viewer = new hrt.impl.DomkitViewer(style, hxd.Res.comp.button, s2d);
-		viewer.addVariables(hxd.Res.vars);
+
+		root = new h2d.Flow(s2d);
+		root.dom = domkit.Properties.create("flow", root);
+		root.fillWidth = root.fillHeight = true;
+		root.layout = Stack;
+		style.addObject(root);
+
+		var viewer = new DomkitViewer(style, hxd.Res.comp.window2, s2d);
 		viewer.addComponentsPath("comp");
+
+		//new Window2(root);
 	}
 
 	override function update(dt:Float) {
